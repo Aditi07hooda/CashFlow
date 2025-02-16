712657ed88aaa6a5f326041280a126f55a78a725
Aditi <108989470+Aditi07hooda@users.noreply.github.com>
2025-02-17 04:26:08 +0530

    limit req body size

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ import budgetRoutes from "../controllers/BudgetController.js"
 
 const app = express()
 
-app.use(express.json());
+app.use(express.json({ limit: "15mb" })); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
