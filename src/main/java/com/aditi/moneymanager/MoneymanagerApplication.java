f13dc11fab0df74d4e5d3f6d21455fad17c6fa09
Aditi <108989470+Aditi07hooda@users.noreply.github.com>
2025-09-21 18:16:07 +0530

    env

--- src/main/java/com/aditi/moneymanager/MoneymanagerApplication.java
+++ src/main/java/com/aditi/moneymanager/MoneymanagerApplication.java
@@ -10,16 +10,23 @@ public class MoneymanagerApplication {
 
 	public static void main(String[] args) {
 		String activeProfile = System.getProperty("spring.profiles.active", "local");
+
 		if (activeProfile.equals("local")) {
+			// Only executed locally
 			Dotenv dotenv = Dotenv.configure()
 					.ignoreIfMissing()
 					.load();
 
-			System.setProperty("DB_URL", dotenv.get("DB_URL"));
-			System.setProperty("DB_USER", dotenv.get("DB_USER"));
-			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-			System.setProperty("JWT_KEY", dotenv.get("JWT_KEY"));
-			System.setProperty("JWT_EXPIRY", dotenv.get("JWT_EXPIRY"));
+			if (dotenv.get("DB_URL") != null)
+				System.setProperty("DB_URL", dotenv.get("DB_URL"));
+			if (dotenv.get("DB_USER") != null)
+				System.setProperty("DB_USER", dotenv.get("DB_USER"));
+			if (dotenv.get("DB_PASSWORD") != null)
+				System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+			if (dotenv.get("JWT_KEY") != null)
+				System.setProperty("JWT_KEY", dotenv.get("JWT_KEY"));
+			if (dotenv.get("JWT_EXPIRY") != null)
+				System.setProperty("JWT_EXPIRY", dotenv.get("JWT_EXPIRY"));
 		}
 
 		SpringApplication.run(MoneymanagerApplication.class, args);
