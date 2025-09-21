eb42b237c3e49153db529d1b991737a79904b6b0
Aditi <108989470+Aditi07hooda@users.noreply.github.com>
2025-09-21 17:54:48 +0530

    env file config

--- src/main/java/com/aditi/moneymanager/MoneymanagerApplication.java
+++ src/main/java/com/aditi/moneymanager/MoneymanagerApplication.java
@@ -9,13 +9,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class MoneymanagerApplication {
 
 	public static void main(String[] args) {
+		String activeProfile = System.getProperty("spring.profiles.active", "local");
+		if (activeProfile.equals("local")) {
+			Dotenv dotenv = Dotenv.configure()
+					.ignoreIfMissing()
+					.load();
 
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("JWT_KEY", dotenv.get("JWT_KEY"));
-		System.setProperty("JWT_EXPIRY", dotenv.get("JWT_EXPIRY"));
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+			System.setProperty("DB_USER", dotenv.get("DB_USER"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+			System.setProperty("JWT_KEY", dotenv.get("JWT_KEY"));
+			System.setProperty("JWT_EXPIRY", dotenv.get("JWT_EXPIRY"));
+		}
 
 		SpringApplication.run(MoneymanagerApplication.class, args);
 	}
